29cc44616792cbec7eafb7216cc1362912a73ad3
Andrea Bianchi <bianan96@gmail.com>
2022-02-23 17:01:03 +0100

    interfare introduction

---
+++ interfaces/interfaces.ts
@@ -0,0 +1,23 @@
+interface Vehicle {
+    name: string,
+    year: number,
+    broken: boolean,
+    summary(): string
+}
+
+const oldCivic: Vehicle = {
+    name: 'civic',
+    year: 2000,
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`;
+    }
+};
+
+const printVehicle = (vehicle: Vehicle): void => {
+    console.log(vehicle.summary());
+};
+
+printVehicle(oldCivic);
+
+// functions and interfaces
