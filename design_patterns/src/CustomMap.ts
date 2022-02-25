f52a4f2610855d6e82d02e77e01878926d801e49
Andrea Bianchi <bianan96@gmail.com>
2022-02-25 11:59:32 +0100

    create costumMap class

---
+++ design_patterns/src/CustomMap.ts
@@ -0,0 +1,43 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+// Instructions to every other class
+// on how they can be an argumento to 'addMarker'
+interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    };
+}
+
+export class CustomMap {
+    private googleMap: google.maps.Map;
+
+    constructor(divId: string) {
+        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        });
+    }
+
+    addMarker(mappable: Mappable): void {
+        const marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        });
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: 'Hello there!'
+            });
+
+            infoWindow.open(this.googleMap, marker);
+        });
+    }
+}
