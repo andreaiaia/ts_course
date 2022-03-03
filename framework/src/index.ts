b3a0e34003f25996936b427e9b9ffbc1725e8cf2
Andrea Bianchi <bianan96@gmail.com>
2022-03-03 18:23:35 +0100

    split User in different subclasses

--- framework/src/index.ts
+++ framework/src/index.ts
@@ -1,21 +1,6 @@
 import { User } from "./models/User";
 
-const user = new User({ name: 'giorgio', age: 20});
+// const user = new User({ name: "Giovanni Giorgio 2", age: 20});
 
-user.on('change', () => {
-    console.log('change run');
-});
+// user.save();
 
-user.on('change', () => {
-    console.log('run again');
-    
-});
-
-user.on('falcon', () => {
-    console.log("that's not how the force works");
-    
-})
-
-user.trigger('change');
-
-user.trigger('falcon');
