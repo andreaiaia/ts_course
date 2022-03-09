d5cc74ea82601acf47b3238c819d7ccdfb4a7098
Andrea Bianchi <bianan96@gmail.com>
2022-03-09 22:30:41 +0100

    finish framework

--- framework/src/index.ts
+++ framework/src/index.ts
@@ -1,14 +1,19 @@
-import { User } from "./models/User";
-import { UserForm } from "./views/UserForm";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
+import { UserList } from "./views/UserList";
 
-const user = User.buildUser({ name: 'GIOVANNI GIORGIO', age: 20});
+const users = new Collection('http://localhost:3000/users', 
+    (json: UserProps) => {
+        return User.buildUser(json);
+    }
+);
 
-const root = document.getElementById('root');
+users.on('change', () => {
+    const root = document.getElementById('root');
 
-if (root) {
-    const userForm = new UserForm(root, user);
-    
-    userForm.render();
-} else {
-    throw new Error('"root" Element not found!');
-}
+    if (root) {
+        new UserList(root, users).render();
+    }
+});
+
+users.fetch();
